e34ae860d036121b323ce8088e2b440c1e82a1e0
Coral Tzubery <tzubery.coral@gmail.com>
2025-04-05 18:00:02 +0300

    updating the app file

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,27 @@ import path from "path";
 import express from "express";
 import { json } from "body-parser";
 import cookieParser from "cookie-parser";
-import { router as apiRouter } from "./routers/api";
+// import { router as apiRouter } from "./routers/api";
+import appointmentRouter from "./routers/api";
 
 const app = express();
 
-app.use((req, _, next) => {
-    console.log(new Date(), req.method, req.url);
-    next();
-});
+// app.use((req, _, next) => {
+//     console.log(new Date(), req.method, req.url);
+//     next();
+// });
 
 app.use(json());
-app.use(cookieParser(process.env.SESSION_SECRET));
+// app.use(cookieParser(process.env.SESSION_SECRET));
 
-app.use("/api", apiRouter);
-app.use(express.static(path.resolve(__dirname, "..", "public")));
-app.use((_, res) => {
-    res.redirect("404.html");
+app.use("/api", appointmentRouter);
+// app.use(express.static(path.resolve(__dirname, "..", "public")));
+// app.use((_, res) => {
+//     res.redirect("404.html");
+// });
+
+app.get("/", (req, res) => {
+    res.send("Appointment Scheduler API is running");
 });
 
 export default app;
